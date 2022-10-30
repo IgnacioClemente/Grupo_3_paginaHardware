28d57796e83ce4f9ef959e81658bc50ae4f2ecb6
hansow <nachoagus@live.com.ar>
2022-10-30 18:17:25 -0300

    agregado de codigo en el app para que corra la pagina

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path'); // UNE DIRECTORIOS
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 app.listen(3000, () => {
     console.log("Server Running on port",3000);
 });
 
-const path = require('path'); // UNE DIRECTORIOS
-
 app.set('views', path.resolve(__dirname, 'views')); 
-//subir la siguiente lìnea
-app.use(express.static(path.resolve(__dirname, '../public')));
