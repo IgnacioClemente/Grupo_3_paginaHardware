a1c8033451bec772b58a1657122d77a0586681c1
Javier Peña <javitigre1974@gmail.com>
2022-10-31 22:55:31 -0300

    agregado de logos

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
-app.listen(3000, () => {
-    console.log("Server Running on port",3000);
-});
+//app.get('/', function (req, res) {
+//  res.send('views/index.html');
+//});
+app.get('views/index');
+app.listen(3000, () => console.log('http://localhost:3000'))
 
 const path = require('path'); // UNE DIRECTORIOS
 
