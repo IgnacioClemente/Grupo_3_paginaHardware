66891c0d85941d7e9d3f45fe92db2ef6862b2f3b
hansow <nachoagus@live.com.ar>
2022-11-02 11:47:32 -0300

    las imagenes ya aparecen en la pagina

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path'); // UNE DIRECTORIOS
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './views/index.html'));
