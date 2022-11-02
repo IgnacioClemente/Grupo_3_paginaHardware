db67abfb18d3d1dd39ccb550226c3a5cdcc36b5b
Javier Peña <javitigre1974@gmail.com>
2022-11-02 12:47:14 -0300

    algunos agregados en css

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path'); // UNE DIRECTORIOS
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './views/index.html'));
+  res.sendFile(path.resolve(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, () => {
