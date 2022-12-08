3bf1f86c56ded1ba8a41bbf0dab7f59e6f3597a4
[Torri] <rtorri@gmail.com>
2022-12-08 13:12:40 -0300

    combio a products todo

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,19 @@
 const listProducts = [
   {
     id: 1,
-    titulo: "Monitor ",
+    name: "Monitor ",
     description: "Monitor Gamer Samsung LF27T350FHLCZB 27”",
     price: "$70.199",
   },
   {
     id: 2,
-    titulo: "pendrive",
+    name: "pendrive",
     description: "Pendrive Kingston 128GB - USB 3.2",
     price: "$5999",
   },
   {
     id: 3,
-    titulo: "Pc ",
+    name: "Pc ",
     description: "PC CPU EXO UNITED GAMERS K7V",
     price: "$249000",
   },
@@ -27,7 +27,7 @@ const mainController = {
     },
     detalle: (req, res) => {
         const id = req.params.id;
-        const products = listProducts.find(plato => plato.id == id);
+        const products = listProducts.find( products => products.id == id);
         res.render("detailProducts", { products });
     }
 }
